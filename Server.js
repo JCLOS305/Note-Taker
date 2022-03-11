3a8b1b897ad91339113c347dfe3d34c727feddbd
jclos305 <jclos305@hotmail.com>
2022-03-11 11:21:35 -0500

    update

--- Server.js
+++ Server.js
@@ -1,99 +1,77 @@
-// Dependencies
-
 const PORT = process.env.PORT || 3001;
 const fs = require('fs');
 const path = require('path');
-const allNotes = require('./db/db.json');
+
 const express = require('express');
 const app = express();
 
-// linked to assets
-app.use(express.static('public'));
+const allNotes = require('./db/db.json');
 
-// Data Parsing 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
-
-// Pull INDEX AND NOTES HTML
-app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "/public/index.html"));
+app.get('/api/notes', (req, res) => {
+    res.json(allNotes.slice(1));
 });
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
-app.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "/public/notes.html"));
-})
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
-
-// GET and POST function set up.
-app.route("/api/notes")
+function createNewNote(body, notesArray) {
+    const newNote = body;
+    if (!Array.isArray(notesArray))
+        notesArray = [];
     
-    
+    if (notesArray.length === 0)
+        notesArray.push(0);
 
-.get(function (req, res) {
-    res.json(database);
-})
+    body.id = notesArray[0];
+    notesArray[0]++;
 
- 
-.post(function (req, res) {
-    let jsonFilePath = path.join(__dirname, "/db/db.json");
-    let newNote = req.body;
-
-    let highestId = 99;
-      
-    for (let i = 0; i < database.length; i++) {
-    let individualNote = database[i];
-
-    if (individualNote.id > highestId) {
-              
-    highestId = individualNote.id;
-    }
-
+    notesArray.push(newNote);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify(notesArray, null, 2)
+    );
+    return newNote;
 }
-     
-    newNote.id = highestId + 1;
-    database.push(newNote)
-        
-        
-    fs.writeFile(jsonFilePath, JSON.stringify(database), function (err) {
-            
-    if (err) {
-    return console.log(err);
-    }
-    console.log("Your note was saved!");
-        });
-        // if information is 'true' note will be saved if not 'err' will return
-        res.json(newNote);
-    });
-    
-    // Delete Function set up.
 
-app.delete("/api/notes/:id", function (req, res) {
-    let jsonFilePath = path.join(__dirname, "/db/db.json");
-        
-    for (let i = 0; i < database.length; i++) {
-    
-    if (database[i].id == req.params.id) {
-    
-    database.splice(i, 1);
-    break;
-    }
-}
-        // JSON file stringify
-fs.writeFileSync(jsonFilePath, JSON.stringify(database), function (err) {
-    
-    if (err) {
-        return console.log(err);
-        } else {
-        console.log("Your note was deleted!");
+app.post('/api/notes', (req, res) => {
+    const newNote = createNewNote(req.body, allNotes);
+    res.json(newNote);
+});
+
+function deleteNote(id, notesArray) {
+    for (let i = 0; i < notesArray.length; i++) {
+        let note = notesArray[i];
+
+        if (note.id == id) {
+            notesArray.splice(i, 1);
+            fs.writeFileSync(
+                path.join(__dirname, './db/db.json'),
+                JSON.stringify(notesArray, null, 2)
+            );
+
+            break;
         }
-    });
-    res.json(database);
+    }
+}
+
+app.delete('/api/notes/:id', (req, res) => {
+    deleteNote(req.params.id, allNotes);
+    res.json(true);
 });
-    
-   // LISTEN (set up server)
-    app.listen(PORT, function () {
-        console.log("App listening on PORT " + PORT);
-    });
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
